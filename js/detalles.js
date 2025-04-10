feb43cc90269bfb830e743ea580886fdf91620a7
juanse68 <163223814+juanse68@users.noreply.github.com>
2025-04-10 20:02:53 +0000

    12

--- js/detalles.js
+++ js/detalles.js
@@ -1,4 +1,7 @@
-function mostrarDetalles(id){
+async function mostrarDetalles(id){
     const app = document.getElementById("app");
+
+    const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1025')
+    const data = await res.json()
     app.innerHTML = "id";
 }
--- js/lista.js
+++ js/lista.js
@@ -12,7 +12,7 @@ let Pokes = "";
 for (let i = 0; i < pokemones.length; i++) {
     let id = pokemones[i].url.split("/")[6];
     Pokes += `
-    <div class="c-lista-pokemon poke-${id}" onclick="mostrarDetalle('${pokemones[i].name}')">
+    <div class="c-lista-pokemon poke-${id}" onclick="mostrarDetalles('${pokemones[i].name}')">
         <p>#${id}</p>
         <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png" width="auto" height="60" loading="lazy" alt="${pokemones[i].name}">
         <p>${pokemones[i].name}</p>
